9a7716b84db581dfb922f0f855545de0b44af881
Marc Poppy <poppy.marc@gmail.com>
2019-05-10 04:46:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,11 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  else
-    
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+
   end
 
 end
